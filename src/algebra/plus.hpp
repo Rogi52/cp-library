39e2fa8525959d28d986847cf35585a7584a1b14
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-02-20 16:07:35 +0900

    Update plus.hpp

--- src/algebra/plus.hpp
+++ src/algebra/plus.hpp
@@ -3,9 +3,10 @@ namespace Algebra {
 template < class T > class Plus {
   public:
     using set = T;
-    static T op(const T &l, const T &r) { return l + r; }
-    static T id = T(0);
-    static T inv(const T &x) { return -x; }
+    static constexpr T op(const T &l, const T &r) { return l + r; }
+    static constexpr T id = T(0);
+    static constexpr T inv(const T &x) { return -x; }
+    static constexpr bool commute = true;
 };
-
+  
 }
