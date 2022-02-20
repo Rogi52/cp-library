a970ef2c11a32ae24e34c5a1a9177d9c750e439f
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-02-20 17:19:30 +0900

    Update fenwick_tree.hpp

--- src/data_structure/fenwick_tree.hpp
+++ src/data_structure/fenwick_tree.hpp
@@ -1,29 +1,36 @@
-template < class CMonoid > class FenwickTree {
+template < class AbelGroup > class FenwickTree {
   public:
-    using T = typename CMonoid::set;
+    using T = typename AbelGroup::set;
 
   private:
     int n;
-    vector<T> data;
+    vector< T > data;
 
   public:
     FenwickTree() : n(0) {}
-    FenwickTree(int n) : n(n), data(n, CMonoid::id) {}
+    FenwickTree(int n) : n(n), data(n, AbelGroup::id) { assert(AbelGroup::commute); }
+    FenwickTree(vector< T > a) : n(a.size()), data(a) {
+        assert(AbelGroup::commute);
+        for(int i = 1; i <= n; i++) {
+            int p = i + (i & -i);
+            if(p <= n) data[p - 1] = AbelGroup::op(data[i - 1], data[p - 1]);
+        }
+    }
 
     void add(int i, T x) {
-        for(int p = i + 1; p <= n; p += p & -p) data[p - 1] = CMonoid::op(data[p - 1], x);
+        for(int p = i + 1; p <= n; p += p & -p) data[p - 1] = AbelGroup::op(data[p - 1], x);
     }
 
     // [0, r)
     T fold(int r) {
-        T s = CMonoid::id;
-        for(int p = r; p > 0; p -= p & -p) s = CMonoid::op(data[p - 1], s);
+        T s = AbelGroup::id;
+        for(int p = r; p > 0; p -= p & -p) s = AbelGroup::op(data[p - 1], s);
         return s;
     }
-  
+
     // [l, r)
     T fold(int l, int r) {
-        return CMonoid::op(CMonoid::inv(fold(l)), fold(r));
+        return AbelGroup::op(AbelGroup::inv(fold(l)), fold(r));
     }
 
     T get(int i) {
@@ -31,6 +38,6 @@ template < class CMonoid > class FenwickTree {
     }
 
     void set(int i, T x) {
-        add(i, CMonoid::op(CMonoid::inv(get(i)), x));
+        add(i, AbelGroup::op(AbelGroup::inv(get(i)), x));
     }
 };
