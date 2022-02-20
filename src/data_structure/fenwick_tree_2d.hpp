e0503e9baf6e28587c7f1f0b2c00bb9caa938729
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-02-20 18:17:13 +0900

    Update fenwick_tree_2d.hpp

--- src/data_structure/fenwick_tree_2d.hpp
+++ src/data_structure/fenwick_tree_2d.hpp
@@ -13,12 +13,30 @@ template < class AbelGroup > class FenwickTree2D {
                 data[i][j].assign(h + 1, vector< T >(w + 1, AbelGroup::id));
     }
 
+    // [0, x) * [0, y)
+    void add(int x, int y, T v) {
+        sub_add(0, 0, x, y, AbelGroup::pow(v, x * y));
+        sub_add(1, 0, x, y, AbelGroup::pow(AbelGroup::inv(v), x));
+        sub_add(0, 1, x, y, AbelGroup::pow(AbelGroup::inv(v), y));
+        sub_add(1, 1, x, y, v);
+    }
+
     // [x1, x2) * [y1, y2)
     void add(int x1, int x2, int y1, int y2, T v) {
         add(x1, y1, v);
         add(x1, y2, AbelGroup::inv(v));
         add(x2, y1, AbelGroup::inv(v));
         add(x2, y2, v);
+    }
+
+    // [0, x) * [0, y)
+    T fold(int x, int y) {
+        T s = AbelGroup::id;
+        s = AbelGroup::op(sub_fold(0, 0, x, y), s);
+        s = AbelGroup::op(AbelGroup::pow(sub_fold(1, 0, x, y), y), s);
+        s = AbelGroup::op(AbelGroup::pow(sub_fold(0, 1, x, y), x), s);
+        s = AbelGroup::op(AbelGroup::pow(sub_fold(1, 1, x, y), x * y), s);
+        return s;
     }
 
     // [x1, x2) * [y1, y2)
@@ -37,28 +55,12 @@ template < class AbelGroup > class FenwickTree2D {
             for(int j = y + 1; j <= w; j += j & -j)
                 data[f][g][i - 1][j - 1] = AbelGroup::op(data[f][g][i - 1][j - 1], v);
     }
-
-    void add(int x, int y, T v) {
-        sub_add(0, 0, x, y, AbelGroup::pow(v, x * y));
-        sub_add(1, 0, x, y, AbelGroup::pow(AbelGroup::inv(v), x));
-        sub_add(0, 1, x, y, AbelGroup::pow(AbelGroup::inv(v), y));
-        sub_add(1, 1, x, y, v);
-    }
-
+    
     T sub_fold(int f, int g, int x, int y) {
         T s = AbelGroup::id;
         for(int i = x; i > 0; i -= i & -i)
             for(int j = y; j > 0; j -= j & -j)
                 s = AbelGroup::op(data[f][g][i - 1][j - 1], s);
-        return s;
-    }
-
-    T fold(int x, int y) {
-        T s = AbelGroup::id;
-        s = AbelGroup::op(sub_fold(0, 0, x, y), s);
-        s = AbelGroup::op(AbelGroup::pow(sub_fold(1, 0, x, y), y), s);
-        s = AbelGroup::op(AbelGroup::pow(sub_fold(0, 1, x, y), x), s);
-        s = AbelGroup::op(AbelGroup::pow(sub_fold(1, 1, x, y), x * y), s);
         return s;
     }
 };
