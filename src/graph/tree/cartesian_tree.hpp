4e84ee1cb6c9c2fe685f2bbd25eee1e6a4a71e2c
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-03-15 13:32:04 +0900

    Update src/graph/tree/cartesian_tree.hpp

--- src/graph/tree/cartesian_tree.hpp
+++ src/graph/tree/cartesian_tree.hpp
@@ -4,17 +4,14 @@ template < class T >
 pair< vector<vector<int>>, int > cartesian_tree(vector< T > &a) {
     int n = a.size();
     vector<vector<int>> graph(n);
-    vector<int> p(n, -1), st;
-    st.reserve(n);
+    vector<int> p(n, -1);
+    stack<int> st;
     for(int i = 0; i < n; i++) {
         int prev = -1;
-        while(!st.empty() && a[i] < a[st.back()]) {
-            prev = st.back();
-            st.pop_back();
-        }
+        while(!st.empty() && a[i] < a[st.top()]) { prev = st.top(); st.pop(); }
         if(prev != -1) p[prev] = i;
-        if(!st.empty()) p[i] = st.back();
-        st.push_back(i);
+        if(!st.empty()) p[i] = st.top();
+        st.push(i);
     }
 
     int root = -1;
