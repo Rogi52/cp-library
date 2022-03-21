56348fd19ad90d0a6b268eed6e2ed82571de759f
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-03-21 11:24:33 +0900

    Create find_local_min.hpp

---
+++ src/algorithm/find_local_min.hpp
@@ -0,0 +1,14 @@
+// f: [0, n) -> T
+template < class T, class func > pair< int, T > find_local_min(func f, int n) {
+    int a = 0, b = (n + 1) / 2, c = n + 1;
+    while(c - a > 2) {
+        int l = (a + b) / 2, r = (b + c + 1) / 2;
+        if(f(l) < f(b))
+            tie(a, b, c) = make_tuple(a, l, b);
+        else if(f(b) > f(r))
+            tie(a, b, c) = make_tuple(b, r, c);
+        else
+            tie(a, b, c) = make_tuple(l, b, r);
+    }
+    return {b, f(b)};
+};
