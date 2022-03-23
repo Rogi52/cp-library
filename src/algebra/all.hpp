494521845d781f0700c3453aa0ab71583275ec99
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-03-23 19:18:32 +0900

    Update src/algebra/all.hpp

--- src/algebra/all.hpp
+++ src/algebra/all.hpp
@@ -1,6 +1,6 @@
 namespace Algebra {
 
-template < class T > class Plus {
+template < class T > class PLUS {
   public:
     using set = T;
     static constexpr T op(const T &l, const T &r) { return l + r; }
