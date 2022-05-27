3c25a29be5a54911022f861f39a94b2f879dd119
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-05-27 13:17:08 +0900

    Update fenwick_tree.hpp

--- src/data_structure/fenwick_tree.hpp
+++ src/data_structure/fenwick_tree.hpp
@@ -8,9 +8,8 @@ template < class abel_group > class fenwick_tree {
 
   public:
     fenwick_tree() : n(0) {}
-    fenwick_tree(int n) : n(n), data(n, abel_group::id) { assert(abel_group::commute); }
+    fenwick_tree(int n) : n(n), data(n, abel_group::id) {}
     fenwick_tree(vector< T > a) : n(a.size()), data(a) {
-        assert(abel_group::commute);
         for(int i = 1; i <= n; i++) {
             int p = i + (i & -i);
             if(p <= n) data[p - 1] = abel_group::op(data[i - 1], data[p - 1]);
@@ -20,24 +19,23 @@ template < class abel_group > class fenwick_tree {
     void add(int i, T x) {
         for(int p = i + 1; p <= n; p += p & -p) data[p - 1] = abel_group::op(data[p - 1], x);
     }
-
     // [0, r)
     T fold(int r) {
         T s = abel_group::id;
         for(int p = r; p > 0; p -= p & -p) s = abel_group::op(data[p - 1], s);
         return s;
     }
-
     // [l, r)
     T fold(int l, int r) {
+        assert(abel_group::comm);
         return abel_group::op(abel_group::inv(fold(l)), fold(r));
     }
-
     T get(int i) {
+        assert(abel_group::comm);
         return fold(i, i + 1);
     }
-
     void set(int i, T x) {
+        assert(abel_group::comm);
         add(i, abel_group::op(abel_group::inv(get(i)), x));
     }
 };
