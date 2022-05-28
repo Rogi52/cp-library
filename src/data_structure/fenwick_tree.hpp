ea8ec1621a4b0c0b196163773a173b2aaf631c4b
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-05-28 14:11:37 +0900

    Update fenwick_tree.hpp
    + 1-indexed
    + serach(lambda)

--- src/data_structure/fenwick_tree.hpp
+++ src/data_structure/fenwick_tree.hpp
@@ -14,22 +14,23 @@ template < class comm_monoid > class fenwick_tree {
 
   public:
     fenwick_tree() : fenwick_tree(0) {}
-    fenwick_tree(int n) : n(n), n2(ceil_pow2(n)) data(n, comm_monoid::id) { assert(comm_monoid::comm); }
+    fenwick_tree(int n) : n(n), n2(ceil_pow2(n)), data(n + 1, comm_monoid::id) { assert(comm_monoid::comm); }
     fenwick_tree(const vector< T > &a) : n(a.size()), n2(ceil_pow2(n)), data(a) {
         assert(comm_monoid::comm);
+        data.insert(a.begin(), {comm_monoid::id});
         for(int i = 1; i <= n; i++) {
             int p = i + (i & -i);
-            if(p <= n) data[p - 1] = comm_monoid::op(data[i - 1], data[p - 1]);
+            if(p <= n) data[p] = comm_monoid::op(data[i], data[p]);
         }
     }
 
     void add(int i, T x) {
-        for(int p = i + 1; p <= n; p += p & -p) data[p - 1] = comm_monoid::op(data[p - 1], x);
+        for(int p = i + 1; p <= n; p += p & -p) data[p] = comm_monoid::op(data[p], x);
     }
     // [0, r)
     T fold(int r) {
         T s = comm_monoid::id;
-        for(int p = r; p > 0; p -= p & -p) s = comm_monoid::op(data[p - 1], s);
+        for(int p = r; p > 0; p -= p & -p) s = comm_monoid::op(data[p], s);
         return s;
     }
     // [l, r)
@@ -42,14 +43,15 @@ template < class comm_monoid > class fenwick_tree {
     void set(int i, T x) {
         add(i, comm_monoid::op(comm_monoid::inv(get(i)), x));
     }
-    int lower_bound(function<bool(T)> &f) {
+    template< class func > int search(const func &f) {
         T s = comm_monoid::id;
         if(f(s)) return 0;
         int i = 0, k = n2;
         while(k >>= 1) {
-            if((i | k) <= n && !f(comm_monoid::op(s, data[i | k])))
-                s = comm_monoid::op(s, data[i |= k]);
+            int p = i | k;
+            if(p <= n && !f(comm_monoid::op(s, data[p])))
+                s = comm_monoid::op(s, data[i = p]);
         }
-        return i + 1;
+        return i;
     }
 };
