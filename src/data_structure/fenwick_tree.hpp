68365dea4d4ee6c7a7bc2552fef534f293b17f7b
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-05-28 12:48:06 +0900

    Update fenwick_tree.hpp
    - abel_monoid
    + comm_monoid
    + lower_bound

--- src/data_structure/fenwick_tree.hpp
+++ src/data_structure/fenwick_tree.hpp
@@ -1,41 +1,55 @@
-template < class abel_group > class fenwick_tree {
+template < class comm_monoid > class fenwick_tree {
   public:
-    using T = typename abel_group::set;
+    using T = typename comm_monoid::set;
 
   private:
-    int n;
+    int n, n2;
     vector< T > data;
 
+    int ceil_pow2(int n) {
+        int x = 1;
+        while(x < n) x <<= 1;
+        return x;
+    }
+
   public:
-    fenwick_tree() : n(0) {}
-    fenwick_tree(int n) : n(n), data(n, abel_group::id) {}
-    fenwick_tree(vector< T > a) : n(a.size()), data(a) {
+    fenwick_tree() : fenwick_tree(0) {}
+    fenwick_tree(int n) : n(n), n2(ceil_pow2(n)) data(n, comm_monoid::id) { assert(comm_monoid::comm); }
+    fenwick_tree(const vector< T > &a) : n(a.size()), n2(ceil_pow2(n)), data(a) {
+        assert(comm_monoid::comm);
         for(int i = 1; i <= n; i++) {
             int p = i + (i & -i);
-            if(p <= n) data[p - 1] = abel_group::op(data[i - 1], data[p - 1]);
+            if(p <= n) data[p - 1] = comm_monoid::op(data[i - 1], data[p - 1]);
         }
     }
 
     void add(int i, T x) {
-        for(int p = i + 1; p <= n; p += p & -p) data[p - 1] = abel_group::op(data[p - 1], x);
+        for(int p = i + 1; p <= n; p += p & -p) data[p - 1] = comm_monoid::op(data[p - 1], x);
     }
     // [0, r)
     T fold(int r) {
-        T s = abel_group::id;
-        for(int p = r; p > 0; p -= p & -p) s = abel_group::op(data[p - 1], s);
+        T s = comm_monoid::id;
+        for(int p = r; p > 0; p -= p & -p) s = comm_monoid::op(data[p - 1], s);
         return s;
     }
     // [l, r)
     T fold(int l, int r) {
-        assert(abel_group::comm);
-        return abel_group::op(abel_group::inv(fold(l)), fold(r));
+        return comm_monoid::op(comm_monoid::inv(fold(l)), fold(r));
     }
     T get(int i) {
-        assert(abel_group::comm);
         return fold(i, i + 1);
     }
     void set(int i, T x) {
-        assert(abel_group::comm);
-        add(i, abel_group::op(abel_group::inv(get(i)), x));
+        add(i, comm_monoid::op(comm_monoid::inv(get(i)), x));
+    }
+    int lower_bound(function<bool(T)> &f) {
+        T s = comm_monoid::id;
+        if(f(s)) return 0;
+        int i = 0, k = n2;
+        while(k >>= 1) {
+            if((i | k) <= n && !f(comm_monoid::op(s, data[i | k])))
+                s = comm_monoid::op(s, data[i |= k]);
+        }
+        return i + 1;
     }
 };
