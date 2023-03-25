5c528bc80da15244a1a1ad0eb909bbf180cfdcb6
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-26 03:15:04 +0900

    Delete quotient_range.hpp

--- src/number/quotient_range.hpp
+++
@@ -1,8 +0,0 @@
-vector< pair< pair< ll, ll >, ll > > quotient_range(ll N) {
-    vector< pair< pair< ll, ll >, ll > > res;
-    for(ll L = 1; L <= N; L = N / (N / L) + 1) {
-        ll X = N / L, R = N / X;
-        res.push_back({{L, R}, X});
-    }
-    return res;
-}
