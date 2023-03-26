627ac175b7c1244c4688219d7ca3e84a71d90080
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-26 14:37:28 +0900

    Create segtree.test.cpp

---
+++ verify/library_checker/data_structure/segtree.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include "src/cp-template.hpp"
+#include "src/data_structure/segtree.hpp"
+#include "src/algebra/affine.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int N,Q; cin >> N >> Q;
+    using F = affine<mint>;
+    vector< F > f(N);
+    rep(i,N) {
+        mint a,b; cin >> a >> b;
+        f[i] = F(a, b);
+    }
+
+    segtree< affine_composite_monoid<mint> > seg(f);
+    rep(_,Q) {
+        int t; cin >> t;
+        if(t == 0) {
+            int p; mint c,d; cin >> p >> c >> d;
+            seg.set(p, F(c, d));
+        }
+
+        if(t == 1) {
+            int l,r; mint x; cin >> l >> r >> x;
+            cout << seg.prod(l, r).eval(x) << "\n";
+        }
+    }
+}
