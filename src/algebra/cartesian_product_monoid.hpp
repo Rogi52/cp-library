3fded808f813758f74cdc1222211651fc993ba97
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-26 15:58:03 +0900

    Create cartesian_product_monoid.hpp

---
+++ src/algebra/cartesian_product_monoid.hpp
@@ -0,0 +1,9 @@
+template < class M, class N > class cartesian_product_monoid {
+    using T = pair< typename M::set, typename N::set >;
+  public:
+    using set = T;
+    static constexpr T op(const T &l, const T &r) {
+        return T(M::op(l.first, r.first), N::op(l.second, r.second));
+    }
+    static constexpr T id{M::id, N::id};
+};
