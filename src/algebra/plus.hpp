bdc38eef3e14d10c753241e8f9914fbdccb49dab
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-26 22:09:46 +0900

    Update plus.hpp

--- src/algebra/plus.hpp
+++ src/algebra/plus.hpp
@@ -1,9 +1,9 @@
 template < class T > class PLUS {
-   public:
-     using set = T;
-     static constexpr T op(const T &l, const T &r) { return l + r; }
-     static constexpr T id = T(0);
-     static constexpr T inv(const T &x) { return -x; }
-     static constexpr T pow(const T &x, const int n) { return x * n; }
-     static constexpr bool comm = true;
- };
+  public:
+    using set = T;
+    static constexpr T op(const T &l, const T &r) { return l + r; }
+    static constexpr T id = T(0);
+    static constexpr T inv(const T &x) { return -x; }
+    static constexpr T pow(const T &x, const int n) { return x * n; }
+    static constexpr bool comm = true;
+};
