3b51f254ce8db92f1fbd361031960febd92407da
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-29 21:46:59 +0900

    Create rolling_hash.test.cpp
    by problem for sa

---
+++ verify/library_checker/string/rolling_hash.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/suffixarray"
+
+#include "src/cp-template.hpp"
+#include "src/string/rolling_hash.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    string s; cin >> s;
+    int n = s.size();
+    rolling_hash< 1 > rh(s);
+    vector<int> I(n);
+    iota(I.begin(), I.end(), 0);
+    sort(I.begin(), I.end(), [&](int i, int j) {
+        return rolling_hash< 1 >::cmp(rh, i, n, rh, j, n) < 0;
+    });
+    rep(i,n) cout << I[i] << " \n"[i == n - 1];
+}
