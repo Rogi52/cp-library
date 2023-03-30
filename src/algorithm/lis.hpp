db133c0acc2118e0f2f6e4a4a12f2e6de290a078
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-31 00:03:58 +0900

    Update lis.hpp

--- src/algorithm/lis.hpp
+++ src/algorithm/lis.hpp
@@ -1,17 +1,17 @@
 template < class T, class Cmp >
 tuple< vector< T >, vector<int>, vector<int> > l_s(const vector< T >& a, Cmp cmp) {
     int n = a.size();
-    vector< T > lis;
+    vector< T > l_s;
     vector<int> st(n, -1), nt(n, -1), rank(n);
     rep(i,n) {
-        int pos = lower_bound(lis.begin(), lis.end(), a[i], cmp) - lis.begin();
+        int pos = lower_bound(l_s.begin(), l_s.end(), a[i], cmp) - l_s.begin();
         st[pos] = i;
         if(pos >= 1) nt[i] = st[pos - 1];
-        if(pos == int(lis.size())) lis.push_back(a[i]); else lis[pos] = a[i];
+        if(pos == int(l_s.size())) l_s.push_back(a[i]); else l_s[pos] = a[i];
         rank[i] = pos + 1;
     }
-    int len = lis.size();
+    int len = l_s.size();
     vector<int> idx(len);
     for(int i = st[len - 1]; i >= 0; i = nt[i]) idx[--len] = i;
-    return {lis, idx, rank};
+    return {l_s, idx, rank};
 }
