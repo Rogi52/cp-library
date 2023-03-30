70d68eb30a75bec7afde5f16b2597e8d5a9212fb
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-30 23:29:10 +0900

    Update lis.hpp

--- src/algorithm/lis.hpp
+++ src/algorithm/lis.hpp
@@ -1,13 +1,17 @@
-template < class T >
-pair< vector< T >, vector<int> > lis(const vector< T >& a, bool strict) {
+template < class T, class Cmp >
+tuple< vector< T >, vector<int>, vector<int> > l_s(const vector< T >& a, Cmp cmp) {
     int n = a.size();
     vector< T > lis;
-    vector<int> res(n);
+    vector<int> rank(n), st(n, -1), nt(n, -1);
     rep(i,n) {
-        auto it = (strict ? lower_bound(lis.begin(), lis.end(), a[i])
-                          : upper_bound(lis.begin(), lis.end(), a[i]));
-        (lis.end() == it ? lis.push_back(a[i]) : *it = a[i]);
-        res[i] = lis.size();
+        int pos = lower_bound(lis.begin(), lis.end(), a[i], cmp) - lis.begin();
+        st[pos] = i;
+        if(pos >= 1) nt[i] = st[pos - 1];
+        if(pos == int(lis.size())) lis.push_back(a[i]); else lis[pos] = a[i];
+        rank[i] = pos + 1;
     }
-    return {lis, res};
+    int len = lis.size();
+    vector<int> idx(len);
+    for(int i = st[len - 1]; i >= 0; i = nt[i]) idx[--len] = i;
+    return {lis, idx, rank};
 }
