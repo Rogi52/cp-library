5eddb8580c09f79135a6901f78bb88362f796382
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-03-31 01:31:22 +0900

    Create count_subseq.test.cpp

---
+++ verify/library_checker/algorithm/count_subseq.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/number_of_subsequences"
+
+#include "src/cp-template.hpp"
+#include "src/number/modint.hpp"
+#include "src/algorithm/count_subseq.hpp"
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int N; cin >> N;
+    vector<int> A(N);
+    rep(i,N) cin >> A[i];
+    cout << count_subseq<mint>(A) - 1 << endl;
+}
