7bf32bea0af13dea4ee73a141ba0e677113ef69c
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-05-06 08:26:47 +0900

    Update cp-template.hpp

--- src/cp-template.hpp
+++ src/cp-template.hpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
 using ld = long double;
 using uint = unsigned int;
 using ull  = unsigned long long;
+using i128 = __int128_t;
 template < class T > bool chmin(T& a, T b) { if(a > b) { a = b; return true; } return false; }
 template < class T > bool chmax(T& a, T b) { if(a < b) { a = b; return true; } return false; }
