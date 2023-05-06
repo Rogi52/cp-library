956092b0863e54c66987177998d1e961feb7561e
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-05-06 10:54:11 +0900

    Update count_subseq.hpp

--- src/algorithm/count_subseq.hpp
+++ src/algorithm/count_subseq.hpp
@@ -4,7 +4,7 @@ mint count_subseq(const vector< T >& a) {
     unordered_map< T, int > mp;
     vector<mint> dp(n + 1);
     dp[0] = 1;
-    rep(i,n) {
+    for(int i : rep(n)) {
         dp[i + 1] = dp[i] + dp[i];
         if(mp.count(a[i])) dp[i + 1] -= dp[mp[a[i]]];
         mp[a[i]] = i;
