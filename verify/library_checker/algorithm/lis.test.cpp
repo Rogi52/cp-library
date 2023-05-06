c66d16c1b9e8b3f62f27f6f123ebfd317ee5633b
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-05-06 10:20:10 +0900

    Update lis.test.cpp

--- verify/library_checker/algorithm/lis.test.cpp
+++ verify/library_checker/algorithm/lis.test.cpp
@@ -4,15 +4,10 @@
 #include "src/algorithm/lis.hpp"
 
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    
-    int N; cin >> N;
-    vector<int> A(N);
-    rep(i,N) cin >> A[i];
+    int N = in();
+    vector<int> A = in(N);
 
     auto [lis, idx, rank] = l_s(A, [&](int a, int b) { return a < b; });
-    int K = idx.size();
-    cout << K << "\n";
-    rep(i,K) cout << idx[i] << " \n"[i == K - 1];
+    print(idx.size());
+    print(idx);
 }
