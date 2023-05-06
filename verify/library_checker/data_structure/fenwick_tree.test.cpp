d56e5e4913c48c471de006243468dbf205eada11
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-05-06 10:21:49 +0900

    Update fenwick_tree.test.cpp

--- verify/library_checker/data_structure/fenwick_tree.test.cpp
+++ verify/library_checker/data_structure/fenwick_tree.test.cpp
@@ -5,25 +5,24 @@
 #include "src/algebra/plus.hpp"
 
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    
-    int N,Q; cin >> N >> Q;
-    vector<ll> a(N);
-    rep(i,N) cin >> a[i];
+    int N = in();
+    int Q = in();
+    vector<ll> a = in(N);
     fenwick_tree< PLUS< ll > > tree(a);
 
-    rep(_,Q) {
-        int t; cin >> t;
+    for(int _ : rep(Q)) {
+        int t = in();
         switch(t) {
             case 0: {
-                int p,x; cin >> p >> x;
+                int p = in();
+                int x = in();
                 tree.add(p, x);
             } break;
 
             case 1: {
-                int l,r; cin >> l >> r;
-                cout << tree.fold(l, r) << '\n';
+                int l = in();
+                int r = in();
+                print(tree.fold(l, r));
             }
         }
     }
