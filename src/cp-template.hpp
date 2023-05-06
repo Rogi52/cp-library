b7dea94d443947f598f22c08060dbab1bfd43373
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-05-06 10:51:50 +0900

    Update cp-template.hpp

--- src/cp-template.hpp
+++ src/cp-template.hpp
@@ -8,5 +8,5 @@ using i128 = __int128_t;
 template < class T > bool chmin(T& a, T b) { if(a > b) { a = b; return true; } return false; }
 template < class T > bool chmax(T& a, T b) { if(a < b) { a = b; return true; } return false; }
 
-#include "src/utility/io.hpp"
 #include "src/utility/rep_itr.hpp"
+#include "src/utility/io.hpp"
