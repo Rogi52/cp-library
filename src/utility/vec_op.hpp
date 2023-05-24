7024b33e17099ae1dcc64a44fbbe1641da6f04e6
rogi52 <91960528+rogi52@users.noreply.github.com>
2023-05-24 23:48:31 +0900

    Update vec_op.hpp

--- src/utility/vec_op.hpp
+++ src/utility/vec_op.hpp
@@ -2,13 +2,13 @@
 template < class T >
 key_val< int, T > max_of(const vector< T >& a) {
     int i = max_element(a.begin(), a.end()) - a.begin();
-    return {a[i], i};
+    return {i, a[i]};
 }
 
 template < class T >
 key_val< int, T > min_of(const vector< T >& a) {
     int i = min_element(a.begin(), a.end()) - a.begin();
-    return {a[i], i};
+    return {i, a[i]};
 }
 
 template < class T >
@@ -19,17 +19,22 @@ T sum_of(const vector< T >& a) {
 }
 
 template < class T >
-vector<int> freq(const vector< T >& a, T L = 0, T R) {
+vector<int> freq_of(const vector< T >& a, T L, T R) {
     vector<int> res(R - L);
     for(const T x : a) res[x - L]++;
     return res;
 }
 
 template < class T >
+vector<int> freq_of(const vector< T >& a, T R) {
+    return freq_of(a, T(0), R);
+}
+
+template < class T >
 struct prefix_sum {
     vector< T > s;
     prefix_sum(const vector< T >& a) : s(a) {
-        s.insert(sum.begin(), T(0));
+        s.insert(s.begin(), T(0));
         for(int i : rep(a.size())) s[i + 1] += s[i];
     }
     // [L, R)
