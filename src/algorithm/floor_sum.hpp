88f941682d4937294ecd45fd6998813d97b0d31c
Rogi <korogi.0502@gmail.com>
2023-08-05 23:53:29 +0900

    floor sum

---
+++ src/algorithm/floor_sum.hpp
@@ -0,0 +1,11 @@
+#include "../cp-template.hpp"
+
+// sum_{i in [0, n)} floor((a * i + b) / m) 
+ll floor_sum(ll n, ll m, ll a, ll b) {
+    if(n == 0) return ll(0);
+    ll ans = 0;
+    ans += (n - 1) * n / 2 * (a / m), a %= m;
+    ans += n * (b / m), b %= m;
+    ll y_max = a * n + b;
+    ans += floor_sum(y_max / m, a, m, y_max % m);
+}
