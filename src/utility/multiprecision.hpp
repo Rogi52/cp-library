a4456bdd6ede761454599e0a58d8f1af2353092f
Keisuke Korogi <91960528+rogi52@users.noreply.github.com>
2024-02-26 12:00:30 +0900

    Create multiprecision.hpp

---
+++ src/utility/multiprecision.hpp
@@ -0,0 +1,4 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+using bigint = boost::multiprecision::cpp_int;
+using bigfloat = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<1024>>;
